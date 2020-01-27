460789ff201a4255acda3934e1e6e09b413165c2
Michael Sisay <ashenafimichael25@gmail.com>
2020-01-27 05:28:41 -0600

    "Started assignment"

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 3, max = 50, message = "location must be between 3 and 50 characters")
+    private String location;
 
+    public Employer(){
+
+    }
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,24 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
+
+    @Size(min = 3, max = 100, message = "skill description must be between 3 and 100 characters")
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Skill(){
+
+    }
 
 }
