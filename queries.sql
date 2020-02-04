a2756676a8a5dbe4bc57a5323c8eb564c30ab06a
Michael Sisay <ashenafimichael25@gmail.com>
2020-02-03 19:02:18 -0600

    "actually done"

--- queries.sql
+++ queries.sql
@@ -1,7 +1,9 @@
 ## Part 1: Test it with SQL
-
+SELECT * FROM techjobs.job;
 ## Part 2: Test it with SQL
+SELECT * FROM techjobs.job inner join techjobs.employer on job.employer_id=employer.id where employer.location = "st. louis";
 
 ## Part 3: Test it with SQL
-
+DROP TABLE techjobs.job;
 ## Part 4: Test it with SQL
+select distinct skill.id,skill.name,skill.description from techjobs.skill inner join job_skills on skill.id = skills_id where jobs_id is not null ORDER BY skill.name ASC;
